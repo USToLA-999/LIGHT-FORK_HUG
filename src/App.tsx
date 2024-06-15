05f49196eeddecd51f4ff62e50923c118fa7eb80
USToLA69 <112379315+USToLA-999@users.noreply.github.com>
2024-06-15 08:20:51 +0530

    updating some shits and creating some pages

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,8 @@ import { RefineKbar, RefineKbarProvider } from "@refinedev/kbar";
 import { useNotificationProvider } from "@refinedev/antd";
 import "@refinedev/antd/dist/reset.css";
 
-import {dataProvider, liveProvider } from './providers'
+import {authProvider, dataProvider, liveProvider } from './providers'
+import {Home, ForgotPassword, Login, Register} from './pages'
 import routerBindings, {
   DocumentTitleHandler,
   UnsavedChangesNotifier,
@@ -29,7 +30,7 @@ function App() {
                     liveProvider={liveProvider}
                     notificationProvider={useNotificationProvider}
                     routerProvider={routerBindings}
-                    // authProvider={}
+                     authProvider={authProvider}
                     options={{
                       syncWithLocation: true,
                       warnWhenUnsavedChanges: true,
@@ -40,6 +41,10 @@ function App() {
                   >
                     <Routes>
                       <Route index element={<WelcomePage />} />
+                      <Route index element={<Home />}/>
+                      <Route path="/register" element={<Register />}/>
+                      <Route path="/login" element={<Login />}/>
+                      <Route path="/forgot-password" element={<ForgotPassword />}/>
                     </Routes>
                     <RefineKbar />
                     <UnsavedChangesNotifier />
--- src/providers/data/customFetch.ts
+++ src/providers/data/customFetch.ts
@@ -1,69 +1,59 @@
-
-import { GraphQLFormattedError } from "graphql";
-import { url } from "inspector";
-
+import type { GraphQLFormattedError } from "graphql";
 
 type Error = {
-    message:string,
-    statusCode: string;
-}
-
-const customFetch = async (url: string, options: RequestInit) => {
-
-    const accessToken = localStorage.getItem('Acess Token');
-
-    const headers = options.headers as Record<string, string>;
-
-    return await fetch(url, {
-        ...options,
-        headers: {
-            ...headers,
-                Authorization: headers?.Authorization  || `Bearer ${accessToken}`,
-                "Content-Type": "application/json",
-                "Apollo-Require-preflight": "true", 
-        }
-    });
-
+  message: string;
+  statusCode: string;
 };
 
+const customFetch = async (url: string, options: RequestInit) => {
+  const accessToken = localStorage.getItem("access_token");
+  const headers = options.headers as Record<string, string>;
 
-const getGraphQLErrors = (body: Record<"errors", GraphQLFormattedError[] | undefined>): Error | null  =>{
-
-    if(!body){
-        return {
-            message:'Unknown Error',
-            statusCode:'INTERNAL_SERVER_ERROR'
-        }
-    }
-
-    if("error" in body) {
-        const errors = body?.errors;
-
-        const messages= errors?.map((error) => error?.message)?.join('');
-
-        const code = errors?.[0]?.extensions?.code;
-
-        return{
-            message: messages || JSON.stringify(errors),
-            statusCode: code || 500
-        }
-    }
-    return null;
-}
+  return await fetch(url, {
+    ...options,
+    headers: {
+      ...headers,
+      Authorization: headers?.Authorization || `Bearer ${accessToken}`,
+      "Content-Type": "application/json",
+      "Apollo-Require-Preflight": "true",
+    },
+  });
+};
 
 export const fetchWrapper = async (url: string, options: RequestInit) => {
-    const response = await customFetch(url,options);
-    
-    const responseClone = response.clone( );
+  const response = await customFetch(url, options);
 
-    const body = await responseClone.json();
+  const responseClone = response.clone();
+  const body = await responseClone.json();
+  const error = getGraphQLErrors(body);
 
-    const error = getGraphQLErrors(body);
+  if (error) {
+    throw error;
+  }
 
+  return response;
+};
 
-    if(error) {
-        throw error;
-    }
+const getGraphQLErrors = (
+  body: Record<"errors", GraphQLFormattedError[] | undefined>,
+): Error | null => {
+  if (!body) {
+    return {
+      message: "Unknown error",
+      statusCode: "INTERNAL_SERVER_ERROR",
+    };
+  }
 
-    return response;
-}
+  if ("errors" in body) {
+    const errors = body?.errors;
+    const messages = errors?.map((error) => error?.message)?.join("");
+    const code = errors?.[0]?.extensions?.code;
+
+    return {
+      message: messages || JSON.stringify(errors),
+      statusCode: code || 500,
+    };
+  }
+
+  return null;
+};
