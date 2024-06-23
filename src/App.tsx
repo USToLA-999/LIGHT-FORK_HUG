9e294050ff09d295d50b0e964f54c21d8d4a9e6b
USToLA69 <112379315+USToLA-999@users.noreply.github.com>
2024-06-23 11:56:39 +0530

    complete companies features and set the all feature init like create and delete the company

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ import { useNotificationProvider } from "@refinedev/antd";
 import "@refinedev/antd/dist/reset.css";
 
 import {authProvider, dataProvider, liveProvider } from './providers'
-import {Home, ForgotPassword, Login, Register} from './pages'
+import {Home, ForgotPassword, Login, Register, CompanyList} from './pages'
 import routerBindings, {
   CatchAllNavigate,
   DocumentTitleHandler,
@@ -15,6 +15,7 @@ import { App as AntdApp } from "antd";
 import Layout from "./components/layout";
 import { BrowserRouter, Outlet, Route, Routes } from "react-router-dom";
 import { resources } from "./config/resources";
+import Create from "./pages/company/create";
 
 
 function App() {
@@ -59,6 +60,11 @@ function App() {
                         }
                       >
                          <Route index element={<Home />}/>
+                         <Route path="/companies" >
+                            <Route index element = {<CompanyList />} />
+                            <Route path="new" element = {<Create />} />
+                         </Route>
+
                       </Route>
                     </Routes>
                     <RefineKbar />
