3fd8cb5d1f4229e2aedf57de814e41a5d24597fe
USToLA69 <112379315+USToLA-999@users.noreply.github.com>
2024-06-23 15:22:16 +0530

    company option is fully completed adding some edit and delete feature after last commit

--- src/App.tsx
+++ src/App.tsx
@@ -16,6 +16,7 @@ import Layout from "./components/layout";
 import { BrowserRouter, Outlet, Route, Routes } from "react-router-dom";
 import { resources } from "./config/resources";
 import Create from "./pages/company/create";
+import Edit from "./pages/company/edit";
 
 
 function App() {
@@ -63,6 +64,7 @@ function App() {
                          <Route path="/companies" >
                             <Route index element = {<CompanyList />} />
                             <Route path="new" element = {<Create />} />
+                            <Route path="edit/:id" element = {<Edit />} />
                          </Route>
 
                       </Route>
