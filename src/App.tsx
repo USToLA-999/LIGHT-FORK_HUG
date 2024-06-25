ed354d8f09c8946eedf765adb812580361c5d407
USToLA69 <112379315+USToLA-999@users.noreply.github.com>
2024-06-25 16:38:55 +0530

    the all fratures of task is complete by adding edit deleteing preview features drag and drop features adding popup cards and all and now doing some little bit change

--- src/App.tsx
+++ src/App.tsx
@@ -27,7 +27,7 @@ import TasksEditPage from "./pages/tasks/edit";
 function App() {
   return (
     <BrowserRouter>
-      <GitHubBanner/>
+      
         <RefineKbarProvider>
         
           
